ad4bb9950436e04b309ab151da20d795c721b674
Andrew Hou <andrewfhou@gmail.com>
2018-06-19 20:48:46 -0500

    first commit

---
+++ src/PurePursuit.java
@@ -0,0 +1,66 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class PurePursuit {
+    static class Point {
+        double x, y;
+        Point(double x, double y) { this.x = x; this.y = y; }
+        double[] asArray() { return new double[] { x, y }; }
+        public String toString() { return String.format("[%.2f, %.2f]", x, y); }
+    }
+
+    static class Line {
+        Point pt1;
+        Point pt2;
+        private double angle;
+        private double sinAngle;
+        private double cosAngle;
+
+        Line(Point pt1, Point pt2) {
+            this.pt1 = pt1;
+            this.pt2 = pt2;
+            this.angle = Math.atan2(pt2.y - pt1.y, pt2.x - pt1.x);
+            this.sinAngle = Math.sin(angle);
+            this.cosAngle = Math.cos(angle);
+        }
+
+        double getAngle() { return angle; }
+        double getAngleDeg() { return Math.toDegrees(angle); }
+
+        Point rotate(Point pt) {
+            double xTran = pt.x - pt1.x;
+            double yTran = pt.y - pt1.y;
+            double x, y;
+
+			if (angle > 0.0) { //cw rotation
+                x = (xTran * cosAngle) + (yTran * sinAngle);
+                y = -(xTran * sinAngle) + (yTran * cosAngle);
+			} else { //ccw rotation
+				x = (xTran * cosAngle) - (yTran * sinAngle);
+				y = (xTran * sinAngle) + (yTran * cosAngle);
+			}
+            return new Point(x, y);
+        }
+    }
+
+    public static void main(String[] args) {
+        Point pt1 = new Point(0.0, 0.0);
+        Point pt2 = new Point(10.0, 0.0);
+        Point pt3 = new Point(20.0, 10.0);
+
+        Line ln = new Line(pt1, pt2);
+
+        Point pt2t = ln.rotate(pt2); //todo: fix this shit
+        Point pt3t = ln.rotate(pt3);
+
+        System.out.println("pt1: " + pt1 + "\tpt2: " + pt2);
+        System.out.println("target heading: " + ln.angle + "\n");
+
+        Line lnTwo = new Line(pt2t, pt3t);
+
+        System.out.println("pt2t: " + pt2t + "\tpt3t" + pt3t);
+        System.out.println("target heading: " + lnTwo.angle + "\n");
+
+    }
+
+}
